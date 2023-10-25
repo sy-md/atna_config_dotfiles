7cbbf90153348cdb78d953904dbd018d8bb4cdde
sy-vm <mdorsetti733@gmail.com>
2023-10-25 16:29:07 -0700

    updated the dot file manager

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ import os
 comfirm = input("are you in your Home directory with your dotfiles: y/n ")
 if comfirm == "y":
 
-	os.system("rm -r Athena && yes | cp -rf .config/ .oh-my-zsh/ .vim/ .zshrc ../")
-	os.system("git clone 'https://github.com/sy-md/Athena.git'")
+#	os.system("rm -r Athena && yes | cp -rf .config/ .oh-my-zsh/ .vim/ .zshrc ../")
+#	os.system("git clone 'https://github.com/sy-md/Athena.git'")
 	os.system("git clone 'https://github.com/sy-md/ohmyzsh.git' ")
-	os.system("git clone 'https://github.com/sy-md/nvim.git' ")
-	os.system("mv nvim ../ ")
-	os.sytem("mv ohmyzsh .ohmyzsh && mv .ohmyzsh ../")
+#	os.system("git clone 'https://github.com/sy-md/nvim.git' ")
+#	os.system("mv nvim ../ ")
+#	os.sytem("mv ohmyzsh .ohmyzsh && mv .ohmyzsh ../")
 	
 if comfirm  == "n":
 	exit()
